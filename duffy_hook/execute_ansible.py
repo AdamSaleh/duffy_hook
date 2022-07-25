10c6ea191b16b7dabf5544331a8c783bc9bfe823
Adam Saleh <asaleh@redhat.com>
2022-07-25 15:41:39 +0200

    Making ansible work.

--- duffy_hook/execute_ansible.py
+++ duffy_hook/execute_ansible.py
@@ -13,7 +13,9 @@ def execute_playbook(inventory_string, playbook_path, extra_vars={}):
     context.CLIARGS = ImmutableDict(
         tags={}, listtags=False, listtasks=False, listhosts=False,
         syntax=False, connection='ssh', module_path=None, forks=100,
-        remote_user='root', private_key_file=None, ssh_common_args=None,
+        remote_user='root',
+        private_key_file="/etc/duffy-ssh-key/ssh-privatekey",
+        ssh_common_args=None,
         ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None,
         become=True, become_method='sudo', become_user='root',
         verbosity=True, check=False, start_at_task=None,
