c141b4edb1d153a8ae341954868239e92eda2c59
Adam Saleh <asaleh@redhat.com>
2022-08-29 14:40:33 +0200

    Added scheduler

--- duffy_hook/main.py
+++ duffy_hook/main.py
@@ -1,10 +1,13 @@
 import json
 import os
+import time
 from typing import Optional
 
+import schedule
 import uvicorn
 from cicoclient.wrapper import CicoWrapper
 from execute_ansible import execute_playbook
+from fabric import Connection
 from fastapi import FastAPI
 from fastapi import Header
 from fastapi import Request
@@ -17,11 +20,13 @@ api = CicoWrapper(
 )
 
 
-def enroll_duffy_as_action(labels, version="8-stream"):
+def enroll_duffy_as_action(extra_vars, labels, version="8-stream"):
+    arch = "x86_64"
     print("Request with labels:", labels)
 
+    # todo what if fails
     hosts, ssid = api.node_get(
-        arch="x86_64",
+        arch=arch,
         ver=version,
         count=1,
         retry_count=2,
@@ -31,11 +36,9 @@ def enroll_duffy_as_action(labels, version="8-stream"):
     key, host = hosts.popitem()
     print(key)
     print(host)
-    extra_vars = {}
-    with open('/etc/duffy-hook/ansible_config.json') as fs:
-        extra_vars = json.load(fs)
     extra_vars["github_runner_labels"] = ",".join(labels)
 
+    # todo what if fails
     result = execute_playbook(
         inventory_string=f"{host['ip_address']},",
         extra_vars=extra_vars,
@@ -43,6 +46,49 @@ def enroll_duffy_as_action(labels, version="8-stream"):
     )
     print(result)
     return result
+
+
+def clear_counts(config):
+    for k in config:
+        for v in config[k]['versions']:
+            v['current_count'] = 0
+    return config
+
+
+def make_sure__there_is_enough_nodes(config):
+    # get current nodes
+    nodes = api.inventory()
+    nodes = update_nodes_github_runner_info(nodes)
+    clear_counts(config)
+    # make each node counted in the project
+    for n in nodes:
+        node = nodes[n]
+        for running in node['github']['running']:
+            v = node['centos_version']
+            if v in config[running]['version']:
+                config[running]['version'][v]['current_count'] += 1
+
+    # for each project
+    for k in config:
+        for v in config[k]['versions']:
+            if config[k][v]['current_count'] < config[k][v]['min_count']:
+                config[k][v]["runner_slug"] = k
+                enroll_duffy_as_action(
+                    config[k][v], config[k][v]['labels'],
+                    version=v, arch=config[k][v]['arch'],
+                )
+
+
+@app.on_event("startup")
+def schedule_there_is_enough_nodes() -> None:
+    config = {}
+    with open('/etc/duffy-hook/ansible_config.json') as fs:
+        config = json.load(fs)
+    schedule.every(10).minutes.do(make_sure__there_is_enough_nodes, config)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 @app.get('/')
@@ -55,9 +101,12 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {'item_id': item_id, 'q': q}
 
 
-@app.post("/enroll/{label}/{version}")
-async def receive_enroll_duffy(label, version):
-    enroll_duffy_as_action([label], version)
+@app.post("/enroll/{app_name}/{label}/{version}")
+async def receive_enroll_duffy(app_name, label, version):
+    extra_vars = {}
+    with open('/etc/duffy-hook/ansible_config.json') as fs:
+        extra_vars = json.load(fs)
+    enroll_duffy_as_action(extra_vars[app_name], [label], version)
 
 
 @app.post("/webhook/{app_name}")
@@ -74,7 +123,7 @@ async def receive_payload(
         payload = await request.json()
         labels = payload['workflow_job']['labels']
         if 'stream-8' in labels:
-            enroll_duffy_as_action(labels, "8-stream")
+            enroll_duffy_as_action(app_name, labels, "8-stream")
 # {'n63.pufty': {'host_id': 127, 'hostname': 'n63.pufty',
 # 'ip_address': '172.19.3.127', 'chassis': 'pufty',
 # 'used_count': 4411, 'current_state': 'Deployed',
@@ -91,9 +140,35 @@ async def receive_payload(
         return {"message": "Unable to process action"}
 
 
+def update_nodes_github_runner_info(nodes):
+    for k in nodes:
+        ip = nodes[k]["ip_address"]
+        # getting the project from running process
+        # the service name is the first word, i.e.
+        # actions.runner.AdamSaleh-sig-core-t_functional.localhost.service
+        result_running = Connection(ip).run(
+            "systemctl --no-page"
+            " | grep 'running.*GitHub Actions Runner'"
+            " | awk '{print $1}'"
+            " | awk -F'.' '{print $3}'", hide=True,
+        )
+        github = {}
+        github['running'] = result_running.stdout.split("\n")
+
+        result_all = Connection(ip).run(
+            "systemctl --no-page"
+            " | grep '.*GitHub Actions Runner'"
+            " | awk '{print $1}'", hide=True,
+        )
+        github['all'] = result_all.stdout.split("\n")
+        nodes[k]['github'] = github
+    return nodes
+
+
 @app.get("/nodes")
 async def get_nodes():
     nodes = api.inventory()
+    nodes = update_nodes_github_runner_info(nodes)
     return nodes
 
 
